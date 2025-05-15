d7539a7df7e0de21f3757d2fdbbabde96c7e8dd2
conuko <constantin.unterkofler@jvm.com>
2025-05-15 16:32:39 +0200

    feat(migrations): add transformation migration for BenefitsItem component

---
+++ examples/fim-migrations/21-transform-benefits-item.js
@@ -0,0 +1,28 @@
+import { defineMigration } from "sb-migrate";
+
+export default defineMigration({
+  type: "transform-entries",
+  component: "BenefitsItem",
+  transform: (blok) => {
+    // Transform text field to text_ref blok
+    if (blok.text) {
+      blok.text_ref = [
+        {
+          component: "Text",
+          body: blok.text,
+        },
+      ];
+    }
+
+    // Convert string icon to asset object if needed
+    if (typeof blok.icon === "string") {
+      blok.icon = {
+        id: blok.icon,
+        filename: blok.icon,
+        alt: "",
+      };
+    }
+
+    return blok;
+  },
+});
